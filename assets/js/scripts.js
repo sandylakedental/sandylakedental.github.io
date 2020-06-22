1f55cb2caf2a19ecc2b3e798540120d0b639ec93
sandylakedental <67288444+sandylakedental@users.noreply.github.com>
2020-06-22 18:33:59 -0400

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,7 +8,7 @@ window.addEventListener('scroll', function (e) {
 	last_scroll_position = window.scrollY;
 
 	// Scrolling down
-	if (new_scroll_position < last_scroll_position && last_scroll_position > 40) {
+	if (new_scroll_position < last_scroll_position && last_scroll_position > 90) {
 		header.classList.remove("is-visible");
 		header.classList.add("is-hidden");
 
@@ -32,6 +32,7 @@ window.addEventListener('scroll', function (e) {
 	new_scroll_position = last_scroll_position;
 });
 
+
 // Dropdown menu
 (function (menuConfig) {
     /**
@@ -39,22 +40,18 @@ window.addEventListener('scroll', function (e) {
      */
     var defaultConfig = {
         // behaviour
-        mobileMenuMode: 'overlay',
+        mobileMenuMode: 'sidebar',
         animationSpeed: 300,
         submenuWidth: 300,
         doubleClickTime: 500,
         mobileMenuExpandableSubmenus: false,
-        isHoverMenu: true,
         // selectors
         wrapperSelector: '.navbar',
         buttonSelector: '.navbar__toggle',
         menuSelector: '.navbar__menu',
         submenuSelector: '.navbar__submenu',
         mobileMenuSidebarLogoSelector: null,
-        mobileMenuSidebarLogoUrl: null,
         relatedContainerForOverlayMenuSelector: null,
-        // attributes 
-        ariaButtonAttribute: 'aria-haspopup',
         // CSS classes
         separatorItemClass: 'is-separator',
         parentItemClass: 'has-submenu',
@@ -97,12 +94,6 @@ window.addEventListener('scroll', function (e) {
         } else if (config.mobileMenuMode === 'sidebar') {
             initMobileMenuSidebar();
         }
-
-        initClosingMenuOnClickLink();
-
-        if (!config.isHoverMenu) {
-            initAriaAttributes();
-        }
     };
 
     /**
@@ -112,9 +103,7 @@ window.addEventListener('scroll', function (e) {
         var submenuParents = document.querySelectorAll(config.wrapperSelector + ' .' + config.parentItemClass);
 
         for (var i = 0; i < submenuParents.length; i++) {
-            var eventTrigger = config.isHoverMenu ? 'mouseenter' : 'click';
-
-            submenuParents[i].addEventListener(eventTrigger, function () {
+            submenuParents[i].addEventListener('mouseenter', function () {
                 var submenu = this.querySelector(config.submenuSelector);
                 var itemPosition = this.getBoundingClientRect().left;
                 var widthMultiplier = 2;
@@ -166,13 +155,11 @@ window.addEventListener('scroll', function (e) {
                 submenu.setAttribute('aria-hidden', false);
             });
 
-            if (config.isHoverMenu) {
-                submenuParents[i].addEventListener('mouseleave', function () {
-                    var submenu = this.querySelector(config.submenuSelector);
-                    submenu.removeAttribute('style');
-                    submenu.setAttribute('aria-hidden', true);
-                });
-            }
+            submenuParents[i].addEventListener('mouseleave', function () {
+                var submenu = this.querySelector(config.submenuSelector);
+                submenu.removeAttribute('style');
+                submenu.setAttribute('aria-hidden', true);
+            });
         }
     }
 
@@ -200,7 +187,7 @@ window.addEventListener('scroll', function (e) {
             var relatedContainer = document.querySelector(config.relatedContainerForOverlayMenuSelector);
             menuWrapper.classList.toggle(config.hiddenElementClass);
             button.classList.toggle(config.openedMenuClass);
-            button.setAttribute(config.ariaButtonAttribute, button.classList.contains(config.openedMenuClass));
+            button.setAttribute('aria-expanded', button.classList.contains(config.openedMenuClass));
 
             if (button.classList.contains(config.openedMenuClass)) {
                 document.documentElement.classList.add(config.noScrollClass);
@@ -230,8 +217,6 @@ window.addEventListener('scroll', function (e) {
 
         if (config.mobileMenuSidebarLogoSelector !== null) {
             menuContentHTML = document.querySelector(config.mobileMenuSidebarLogoSelector).outerHTML;
-        } else if (config.mobileMenuSidebarLogoUrl !== null) {
-            menuContentHTML = '<img src="' + config.mobileMenuSidebarLogoUrl + '" alt="" />';
         }
 
         menuContentHTML += document.querySelector(config.menuSelector).outerHTML;
@@ -259,7 +244,7 @@ window.addEventListener('scroll', function (e) {
             menuWrapper.classList.add(config.hiddenElementClass);
             menuOverlay.classList.add(config.hiddenElementClass);
             button.classList.remove(config.openedMenuClass);
-            button.setAttribute(config.ariaButtonAttribute, false);
+            button.setAttribute('aria-expanded', false);
             document.documentElement.classList.remove(config.noScrollClass);
         });
 
@@ -270,8 +255,8 @@ window.addEventListener('scroll', function (e) {
             menuWrapper.classList.toggle(config.hiddenElementClass);
             menuOverlay.classList.toggle(config.hiddenElementClass);
             button.classList.toggle(config.openedMenuClass);
-            button.setAttribute(config.ariaButtonAttribute, button.classList.contains(config.openedMenuClass));
-            document.documentElement.classList.toggle(config.noScrollClass);
+            button.setAttribute('aria-expanded', button.classList.contains(config.openedMenuClass));
+            document.documentElement.classList.add(config.noScrollClass);
         });
     }
 
@@ -315,9 +300,6 @@ window.addEventListener('scroll', function (e) {
                         submenu.removeAttribute('style');
                         submenu.classList.remove(config.openedMenuClass);
                     }, config.animationSpeed);
-
-                    content.setAttribute('aria-hidden', true);
-                    content.parentNode.firstElementChild.setAttribute('aria-expanded', false);
                 } else {
                     var height = content.clientHeight;   
                     submenu.classList.add(config.openedMenuClass);
@@ -330,9 +312,6 @@ window.addEventListener('scroll', function (e) {
                     setTimeout(function () {
                         submenu.removeAttribute('style');
                     }, config.animationSpeed);
-
-                    content.setAttribute('aria-hidden', false);
-                    content.parentNode.firstElementChild.setAttribute('aria-expanded', true);
                 }
             });
 
@@ -353,84 +332,10 @@ window.addEventListener('scroll', function (e) {
                             this.setAttribute('data-last-click', currentTime);
                         } else if (lastClick + config.doubleClickTime > currentTime) {
                             e.stopPropagation();
-                            closeMenu(this, true);
                         }
                     });
                 }
             }
-        }
-    }
-
-    /**
-     * Set aria-* attributes according to the current activity state
-     */
-    function initAriaAttributes () {
-        var allAriaElements = document.querySelectorAll(config.wrapperSelector + ' ' + '*[aria-hidden]');
-
-        for (var i = 0; i < allAriaElements.length; i++) {
-            var ariaElement = allAriaElements[i];
-
-            if (
-                ariaElement.parentNode.classList.contains('active') ||
-                ariaElement.parentNode.classList.contains('active-parent')
-            ) {
-                ariaElement.setAttribute('aria-hidden', 'false');
-                ariaElement.parentNode.firstElementChild.setAttribute('aria-expanded', true);
-            } else {
-                ariaElement.setAttribute('aria-hidden', 'true');
-                ariaElement.parentNode.firstElementChild.setAttribute('aria-expanded', false);
-            }
-        }
-    }
-
-    /**
-     * Close menu on click link
-     */
-    function initClosingMenuOnClickLink () {
-        var links = document.querySelectorAll(config.menuSelector + ' a');
-
-        for (var i = 0; i < links.length; i++) {
-            if (links[i].parentNode.classList.contains(config.parentItemClass)) {
-                continue;
-            }
-
-            links[i].addEventListener('click', function (e) {
-                closeMenu(this, false);
-            });
-        }
-    }
-
-    /**
-     * Close menu
-     */
-    function closeMenu (clickedLink, forceClose) {
-        if (forceClose === false) {
-            if (clickedLink.parentNode.classList.contains(config.parentItemClass)) {
-                return;
-            }
-        }
-
-        var relatedContainer = document.querySelector(config.relatedContainerForOverlayMenuSelector);
-        var button = document.querySelector(config.buttonSelector);
-        var menuWrapper = document.querySelector('.' + config.mobileMenuOverlayClass);
-
-        if (!menuWrapper) {
-            menuWrapper = document.querySelector('.' + config.mobileMenuSidebarClass);
-        }
-
-        menuWrapper.classList.add(config.hiddenElementClass);
-        button.classList.remove(config.openedMenuClass);
-        button.setAttribute(config.ariaButtonAttribute, false);
-        document.documentElement.classList.remove(config.noScrollClass);
-
-        if (relatedContainer) {
-            relatedContainer.classList.remove(config.relatedContainerForOverlayMenuClass);
-        }
-
-        var menuOverlay = document.querySelector('.' + config.mobileMenuSidebarOverlayClass);
-
-        if (menuOverlay) {
-            menuOverlay.classList.add(config.hiddenElementClass);
         }
     }
 
@@ -440,44 +345,6 @@ window.addEventListener('scroll', function (e) {
     init();
 })(window.publiiThemeMenuConfig);
 
-// Load comments
-var comments = document.getElementById("js-comments");  
-   if (comments) {
-      comments.addEventListener("click", function() {   
-          comments.classList.toggle("is-hidden");      
-             var container = document.getElementById("js-comments__inner");   
-             container.classList.toggle("is-visible");  
-      });
- }
-
-// Load comments
-var comments = document.getElementById("js-comments");  
-   if (comments) {
-      comments.addEventListener("click", function() {   
-          comments.classList.toggle("is-hidden");      
-             var container = document.getElementById("js-comments__inner");   
-             container.classList.toggle("is-visible");  
-      });
- }
-
-// Load search input area
-var searchButton = document.querySelector(".js-search-btn");
-    searchOverlay = document.querySelector(".js-search-overlay");
-    searchClose = document.querySelector(".js-search-close");
-    searchInput = document.querySelector(".js-search-input");
-
-if (searchButton) {
-    searchButton.addEventListener("click", function () {        
-        searchOverlay.classList.add("expanded");
-        setTimeout(function() { 
-            searchInput.focus(); 
-        }, 60);        
-    });
-    
-    searchClose.addEventListener("click", function () {
-        searchOverlay.classList.remove('expanded');
-    });
-}
 
 // Share buttons pop-up
 (function () {
@@ -539,74 +406,31 @@ if (searchButton) {
     }
 })();
 
-// Back to Top - by CodyHouse.co on MIT license
-(function(){    
-	var backTop = document.getElementsByClassName('js-footer__bttop')[0],		
-		offset = 600,		
-		offsetOpacity = 1200,
-		scrollDuration = 50,
-		scrolling = false;
-	if( backTop ) {		
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
-			}
-		});
-		backTop.addEventListener('click', function(event) {
-			event.preventDefault();
-			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : scrollTop(scrollDuration);
-		});
-	}
+// Load search input area
+var searchButton = document.querySelector('.js-search-btn');
+    searchOverlay = document.querySelector('.js-search-overlay');
+    searchInput = document.querySelector('.js-search-input');
 
-	function checkBackToTop() {
-		var windowTop = window.scrollY || document.documentElement.scrollTop;
-		( windowTop > offset ) ? addClass(backTop, 'footer__bttop--show') : removeClass(backTop, 'footer__bttop--show', 'footer__bttop--fade-out');
-		( windowTop > offsetOpacity ) && addClass(backTop, 'footer__bttop--fade-out');
-		scrolling = false;
-	}
-	
-	function scrollTop(duration) {
-	    var start = window.scrollY || document.documentElement.scrollTop,
-	        currentTime = null;
-	        
-	    var animateScroll = function(timestamp){
-	    	if (!currentTime) currentTime = timestamp;        
-	        var progress = timestamp - currentTime;
-	        var val = Math.max(Math.easeInOutQuad(progress, start, -start, duration), 0);
-	        window.scrollTo(0, val);
-	        if(progress < duration) {
-	            window.requestAnimationFrame(animateScroll);
-	        }
-	    };
+if (searchButton) {
+    searchButton.addEventListener('click', function () {        
+        searchOverlay.classList.toggle('expanded');
+       
+        setTimeout(function() {
+            if (searchOverlay.classList.contains('expanded')) {
+                searchInput.focus();
+            }
+        }, 60);        
+    });
 
-	    window.requestAnimationFrame(animateScroll);
-	}
+    searchOverlay.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
 
-	Math.easeInOutQuad = function (t, b, c, d) {
- 		t /= d/2;
-		if (t < 1) return c/2*t*t + b;
-		t--;
-		return -c/2 * (t*(t-2) - 1) + b;
-	};
-    
-	function hasClass(el, className) {
-	  	if (el.classList) return el.classList.contains(className);
-	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-	}
-	function addClass(el, className) {
-		var classList = className.split(' ');
-	 	if (el.classList) el.classList.add(classList[0]);
-	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
-	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
-	}
-	function removeClass(el, className) {
-		var classList = className.split(' ');
-	  	if (el.classList) el.classList.remove(classList[0]);	
-	  	else if(hasClass(el, classList[0])) {
-	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
-	  		el.className=el.className.replace(reg, ' ');
-	  	}
-	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
-	}
-})();
+    searchButton.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+
+    document.body.addEventListener('click', function () {
+        searchOverlay.classList.remove('expanded');
+    });
+}
